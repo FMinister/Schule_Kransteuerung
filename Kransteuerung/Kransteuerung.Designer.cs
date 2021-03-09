d8ab3e2c570c4ebb4a1582b6a592ce0a234baa0d
FMinister <56680320+FMinister@users.noreply.github.com>
2021-03-09 11:31:29 +0100

    Initial commit

---
+++ Kransteuerung/Kransteuerung.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Kransteuerung
+{
+    partial class Kransteuerung
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Haken_aus = new System.Windows.Forms.Button();
+            this.Haken_ein = new System.Windows.Forms.Button();
+            this.Ausleger_aus = new System.Windows.Forms.Button();
+            this.Ausleger_ein = new System.Windows.Forms.Button();
+            this.Kran_rechts = new System.Windows.Forms.Button();
+            this.Kran_links = new System.Windows.Forms.Button();
+            this.Kran_aus = new System.Windows.Forms.Button();
+            this.Kran_ein = new System.Windows.Forms.Button();
+            this.Floor = new System.Windows.Forms.Panel();
+            this.Base = new System.Windows.Forms.Panel();
+            this.Haken = new System.Windows.Forms.Panel();
+            this.Arm = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Haken_aus
+            // 
+            this.Haken_aus.Location = new System.Drawing.Point(693, 212);
+            this.Haken_aus.Name = "Haken_aus";
+            this.Haken_aus.Size = new System.Drawing.Size(95, 23);
+            this.Haken_aus.TabIndex = 0;
+            this.Haken_aus.Text = "Haken aus";
+            this.Haken_aus.UseVisualStyleBackColor = true;
+            this.Haken_aus.Click += new System.EventHandler(this.Haken_aus_Click);
+            // 
+            // Haken_ein
+            // 
+            this.Haken_ein.Location = new System.Drawing.Point(693, 241);
+            this.Haken_ein.Name = "Haken_ein";
+            this.Haken_ein.Size = new System.Drawing.Size(95, 23);
+            this.Haken_ein.TabIndex = 1;
+            this.Haken_ein.Text = "Haken ein";
+            this.Haken_ein.UseVisualStyleBackColor = true;
+            this.Haken_ein.Click += new System.EventHandler(this.Haken_ein_Click);
+            // 
+            // Ausleger_aus
+            // 
+            this.Ausleger_aus.Location = new System.Drawing.Point(693, 270);
+            this.Ausleger_aus.Name = "Ausleger_aus";
+            this.Ausleger_aus.Size = new System.Drawing.Size(95, 23);
+            this.Ausleger_aus.TabIndex = 2;
+            this.Ausleger_aus.Text = "Ausleger aus";
+            this.Ausleger_aus.UseVisualStyleBackColor = true;
+            this.Ausleger_aus.Click += new System.EventHandler(this.Ausleger_aus_Click);
+            // 
+            // Ausleger_ein
+            // 
+            this.Ausleger_ein.Location = new System.Drawing.Point(693, 299);
+            this.Ausleger_ein.Name = "Ausleger_ein";
+            this.Ausleger_ein.Size = new System.Drawing.Size(95, 23);
+            this.Ausleger_ein.TabIndex = 3;
+            this.Ausleger_ein.Text = "Ausleger ein";
+            this.Ausleger_ein.UseVisualStyleBackColor = true;
+            this.Ausleger_ein.Click += new System.EventHandler(this.Ausleger_ein_Click);
+            // 
+            // Kran_rechts
+            // 
+            this.Kran_rechts.Location = new System.Drawing.Point(693, 328);
+            this.Kran_rechts.Name = "Kran_rechts";
+            this.Kran_rechts.Size = new System.Drawing.Size(95, 23);
+            this.Kran_rechts.TabIndex = 4;
+            this.Kran_rechts.Text = "Kran rechts";
+            this.Kran_rechts.UseVisualStyleBackColor = true;
+            this.Kran_rechts.Click += new System.EventHandler(this.Kran_rechts_Click);
+            // 
+            // Kran_links
+            // 
+            this.Kran_links.Location = new System.Drawing.Point(693, 357);
+            this.Kran_links.Name = "Kran_links";
+            this.Kran_links.Size = new System.Drawing.Size(95, 23);
+            this.Kran_links.TabIndex = 5;
+            this.Kran_links.Text = "Kran links";
+            this.Kran_links.UseVisualStyleBackColor = true;
+            this.Kran_links.Click += new System.EventHandler(this.Kran_links_Click);
+            // 
+            // Kran_aus
+            // 
+            this.Kran_aus.Location = new System.Drawing.Point(693, 386);
+            this.Kran_aus.Name = "Kran_aus";
+            this.Kran_aus.Size = new System.Drawing.Size(95, 23);
+            this.Kran_aus.TabIndex = 6;
+            this.Kran_aus.Text = "Kran aus";
+            this.Kran_aus.UseVisualStyleBackColor = true;
+            this.Kran_aus.Click += new System.EventHandler(this.Kran_aus_Click);
+            // 
+            // Kran_ein
+            // 
+            this.Kran_ein.Location = new System.Drawing.Point(693, 415);
+            this.Kran_ein.Name = "Kran_ein";
+            this.Kran_ein.Size = new System.Drawing.Size(95, 23);
+            this.Kran_ein.TabIndex = 7;
+            this.Kran_ein.Text = "Kran ein";
+            this.Kran_ein.UseVisualStyleBackColor = true;
+            this.Kran_ein.Click += new System.EventHandler(this.Kran_ein_Click);
+            // 
+            // Floor
+            // 
+            this.Floor.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Floor.Location = new System.Drawing.Point(260, 400);
+            this.Floor.Name = "Floor";
+            this.Floor.Size = new System.Drawing.Size(200, 10);
+            this.Floor.TabIndex = 8;
+            // 
+            // Base
+            // 
+            this.Base.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Base.Location = new System.Drawing.Point(350, 200);
+            this.Base.Name = "Base";
+            this.Base.Size = new System.Drawing.Size(20, 200);
+            this.Base.TabIndex = 9;
+            // 
+            // Haken
+            // 
+            this.Haken.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Haken.Location = new System.Drawing.Point(150, 215);
+            this.Haken.Name = "Haken";
+            this.Haken.Size = new System.Drawing.Size(10, 40);
+            this.Haken.TabIndex = 11;
+            // 
+            // Arm
+            // 
+            this.Arm.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Arm.Location = new System.Drawing.Point(150, 200);
+            this.Arm.Name = "Arm";
+            this.Arm.Size = new System.Drawing.Size(200, 15);
+            this.Arm.TabIndex = 10;
+            // 
+            // Kransteuerung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 441);
+            this.Controls.Add(this.Haken);
+            this.Controls.Add(this.Arm);
+            this.Controls.Add(this.Base);
+            this.Controls.Add(this.Floor);
+            this.Controls.Add(this.Kran_ein);
+            this.Controls.Add(this.Kran_aus);
+            this.Controls.Add(this.Kran_links);
+            this.Controls.Add(this.Kran_rechts);
+            this.Controls.Add(this.Ausleger_ein);
+            this.Controls.Add(this.Ausleger_aus);
+            this.Controls.Add(this.Haken_ein);
+            this.Controls.Add(this.Haken_aus);
+            this.MaximumSize = new System.Drawing.Size(850, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 470);
+            this.Name = "Kransteuerung";
+            this.Text = "Kransteuerung";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Haken_aus;
+        private System.Windows.Forms.Button Haken_ein;
+        private System.Windows.Forms.Button Ausleger_aus;
+        private System.Windows.Forms.Button Ausleger_ein;
+        private System.Windows.Forms.Button Kran_rechts;
+        private System.Windows.Forms.Button Kran_links;
+        private System.Windows.Forms.Button Kran_aus;
+        private System.Windows.Forms.Button Kran_ein;
+        private System.Windows.Forms.Panel Floor;
+        private System.Windows.Forms.Panel Base;
+        private System.Windows.Forms.Panel Haken;
+        private System.Windows.Forms.Panel Arm;
+    }
+}
+
---
+++ Kransteuerung/Kransteuerung.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Kransteuerung
+{
+    public partial class Kransteuerung : Form
+    {
+        private bool _craneLeft;
+
+        public Kransteuerung()
+        {
+            InitializeComponent();
+            _craneLeft = true;
+        }
+
+        private void Haken_aus_Click(object sender, EventArgs e)
+        {
+            if (Haken.Location.Y + Haken.Size.Height < Floor.Location.Y)
+            {
+                Haken.Size = new Size(Haken.Width, Haken.Height + 10);
+            }
+        }
+
+        private void Haken_ein_Click(object sender, EventArgs e)
+        {
+            Haken.Size = new Size(Haken.Width, Haken.Height - 10);
+        }
+
+        private void Ausleger_aus_Click(object sender, EventArgs e)
+        {
+            if (_craneLeft == true)
+            {
+                if (Arm.Location.X > 15)
+                {
+                    Arm.Location = new Point(Arm.Location.X - 10, Arm.Location.Y);
+                    Arm.Size = new Size(Arm.Width + 10, Arm.Height);
+                    Haken.Location = new Point(Haken.Location.X - 10, Haken.Location.Y);
+                }
+            }
+            else
+            {
+                if (Arm.Location.X + Arm.Width < 690)
+                {
+                    Arm.Size = new Size(Arm.Width + 10, Arm.Height);
+                    Haken.Location = new Point(Haken.Location.X + 10, Haken.Location.Y);
+                }
+            }
+        }
+
+        private void Ausleger_ein_Click(object sender, EventArgs e)
+        {
+            if (_craneLeft == true)
+            {
+                if (Arm.Location.X < 350)
+                {
+                    Arm.Location = new Point(Arm.Location.X + 10, Arm.Location.Y);
+                    Arm.Size = new Size(Arm.Width - 10, Arm.Height);
+                    Haken.Location = new Point(Haken.Location.X + 10, Haken.Location.Y);
+                }
+            }
+            else
+            {
+                if (Arm.Location.X > 360)
+                {
+                    Arm.Size = new Size(Arm.Width - 10, Arm.Height);
+                    Haken.Location = new Point(Haken.Location.X - 10, Haken.Location.Y);
+                }
+            }
+        }
+
+        private void Kran_aus_Click(object sender, EventArgs e)
+        {
+            if (Base.Location.Y > 15)
+            {
+                Base.Location = new Point(Base.Location.X, Base.Location.Y - 10);
+                Base.Size = new Size(Base.Width, Base.Height + 10);
+                Arm.Location = new Point(Arm.Location.X, Arm.Location.Y - 10);
+                Haken.Location = new Point(Haken.Location.X, Haken.Location.Y - 10);
+            }
+        }
+
+        private void Kran_ein_Click(object sender, EventArgs e)
+        {
+            if (Base.Location.Y < 350)
+            {
+                Base.Location = new Point(Base.Location.X, Base.Location.Y + 10);
+                Base.Size = new Size(Base.Width, Base.Height - 10);
+                Arm.Location = new Point(Arm.Location.X, Arm.Location.Y + 10);
+                Haken.Location = new Point(Haken.Location.X, Haken.Location.Y + 10);
+                if (Haken.Location.Y + Haken.Size.Height > Floor.Location.Y)
+                {
+                    Haken.Size = new Size(Haken.Width, Haken.Height - 10);
+                }
+            }
+        }
+
+        private void Kran_rechts_Click(object sender, EventArgs e)
+        {
+            if (_craneLeft == true)
+            {
+                _craneLeft = false;
+                Arm.Location = new Point(Arm.Location.X + Arm.Width + 20, Arm.Location.Y);
+                Haken.Location = new Point(Haken.Location.X + 2 * Arm.Width + 10, Haken.Location.Y);
+            }
+        }
+
+        private void Kran_links_Click(object sender, EventArgs e)
+        {
+            if (_craneLeft == false)
+            {
+                _craneLeft = true;
+                Arm.Location = new Point(Arm.Location.X - Arm.Width - 20, Arm.Location.Y);
+                Haken.Location = new Point(Haken.Location.X - 2 * Arm.Width - 10, Haken.Location.Y);
+            }
+        }
+    }
+}
